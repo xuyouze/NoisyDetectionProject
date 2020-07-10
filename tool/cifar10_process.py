4ac485ea86cb84aad4cda102e48ecb818c49f6da
Runqing-forMost <runqingjiang@163.com>
2020-07-10 09:10:11 +0800

    update some data process details

--- tool/cifar10_process.py
+++ tool/cifar10_process.py
@@ -63,7 +63,11 @@ class CIFAR10Process(DataProcess):
         train_data_num = int(Y_raw.shape[0])
         test_data_num = int(Y_test.shape[0])
         for ratio in self.config.noise_ratio:
-            slot = int(10 / int(10 * ratio))
+            if ratio != 0:
+                slot = int(10 / int(10 * ratio))
+            else:
+                slot = train_data_num + 1
+
             train_data_dict = {"data": X_raw, "raw_label": Y_raw}
             test_data_dict = {"data": X_test, "raw_label": Y_test}
 
